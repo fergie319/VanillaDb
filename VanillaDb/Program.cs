8312f371379b50576068ec185258040c27b2b971
Matthew Ferguson <matthew.ferguson@mmfsoftware.ca>
2020-09-07 22:53:57 -0400

    RD-39 - Initial (mostly) empty project creation.

---
+++ VanillaDb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaDb
+{
+    /// <summary>Main Program Class</summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
