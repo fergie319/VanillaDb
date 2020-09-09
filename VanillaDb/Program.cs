c9762b62b5f5a0fe0d43f6dea33f42d83054c197
Matthew Ferguson <matthew.ferguson@mmfsoftware.ca>
2020-09-09 09:46:51 -0400

    RD-39 - Implemented super-simple initialization where it reads the first parameter and opens the sql file to parse.

--- VanillaDb/Program.cs
+++ VanillaDb/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,36 @@ namespace VanillaDb
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("<Table>.sql file must be supplied as the first argument.");
+            }
+
+            // First param is the table file - expect *.sql
+            var sqlFileName = args[0];
+            var sqlFileInfo = new FileInfo(sqlFileName);
+            if (!sqlFileInfo.Exists)
+            {
+                throw new InvalidOperationException($"{sqlFileName} does not exist.");
+            }
+
+            // Open the file and start parsing
+            using (var stream = sqlFileInfo.OpenRead())
+            using (var reader = new StreamReader(stream))
+            {
+                // Read the file one line at a time until the end of the statement to build the list of fields
+                var currentLine = reader.ReadLine();
+                while (currentLine != "GO;")
+                {
+                    // blah blah blah
+                }
+
+                // Read any Indexes by skipping to the ON clause - write warnings about anything that doesn't parse
+            }
+
+            return 0;
         }
     }
 }
