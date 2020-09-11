0ed125d56a74e99c5735768cee1d56496eca6570
Matthew Ferguson <matthew.ferguson@mmfsoftware.ca>
2020-09-11 11:34:20 -0400

    RD-42 - Created empty transform for Insert stored procedures.

---
+++ VanillaDb/InsertProcs/InsertStoredProcCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaDb.Models;
+
+namespace VanillaDb.InsertProcs
+{
+    /// <summary>Partial class supplies parameters to the T4 Template.</summary>
+    public partial class InsertStoredProc
+    {
+        private TableModel Table { get; set; }
+
+        /// <summary>Initializes a new instance of the <see cref="InsertStoredProc"/> class.</summary>
+        /// <param name="table">The table.</param>
+        public InsertStoredProc(TableModel table)
+        {
+            Table = table;
+        }
+    }
+}
