7f41b23145218f99663e7054f0253a5eb8a4dcd1
RFABK\Matthew.Ferguson <Matthew.Ferguson@rfabank.ca>
2022-07-10 09:15:07 -0400

    Added code to write out the TemporalTypes enum for Temporal Query support.

--- VanillaDb/Program.cs
+++ VanillaDb/Program.cs
@@ -157,6 +157,23 @@ namespace " + config.CodeNamespace + @"
 }
 ");
 
+            File.WriteAllText($"{config.OutputCodePath}\\TemporalTypes.cs",
+@"
+namespace " + config.CodeNamespace + @"
+{
+    /// <summary>Enumeration for the types of temporal queries supported by VanillaDb</summary>
+    public enum TemporalTypes
+    {
+        /// <summary>The default behavior - for querying current record.</summary>
+        Default = 0,
+        /// <summary>As of - for querying a record(s) as of a specific date.</summary>
+        AsOf = 1,
+        /// <summary>All - for querying all current and historical data.</summary>
+        All = 2
+    }
+}
+");
+
             return result;
         }
 
