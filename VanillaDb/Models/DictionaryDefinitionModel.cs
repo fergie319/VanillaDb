9d779dd50d008b1b6be859504f5299f881f2b9f3
RFABK\Matthew.Ferguson <Matthew.Ferguson@rfabank.ca>
2022-07-26 17:02:15 -0400

    Added CSVHelper as a dependency and implemented DataDictionaryParser which checks for a CSV file sitting next to each table.sql file in the tables folder and creates extended properties for using as a data dictionary.

---
+++ VanillaDb/Models/DictionaryDefinitionModel.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace VanillaDb.Models
+{
+    /// <summary>Represents the expected CSV for specifying Data Dictionaries for a table.</summary>
+    public class DictionaryDefinitionModel
+    {
+        /// <summary>Gets or sets the field.</summary>
+        [Name("Field")]
+        public string Field { get; set; }
+
+        /// <summary>Gets or sets the description.</summary>
+        [Name("Description")]
+        public string Description { get; set; }
+
+        /// <summary>Gets or sets the values.</summary>
+        [Name("Values")]
+        public string Values { get; set; }
+    }
+}
